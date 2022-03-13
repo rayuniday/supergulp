c9ec773bd2cb2062b2b240ab9fd787f4b40a5906
Rayun <rayuniday@naver.com>
2022-03-13 09:39:43 +0900

    #5.0 Babelify + Browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+// import sass from "gulp-sass";
+import bro from "gulp-bro";
+import babelify from "babelify";
+
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
   pug: {
@@ -14,6 +19,16 @@ const routes = {
     src: "src/img/*",
     dest: "build/img",
   },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js",
+  },
 };
 
 const pug = () =>
@@ -24,17 +39,38 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const watch = () => {
-  gulp.watch(routes.pug.watch, pug);
-};
-
 const img = () => {
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 };
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest([routes.js.dest]));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
+};
+
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles, js]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
